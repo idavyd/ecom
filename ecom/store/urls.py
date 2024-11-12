418ee7e19004a4e608e531237e356269936c5302
idavyd <ihor.davyydenko@gmail.com>
2024-11-12 06:53:14 +0100

    EP 7 11/12 morning session

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home_view(request):
-    return render(request, 'home_view.html', {})
+    products = Product.objects.all()
+    context = {'products': products
+    }
+    return render(request, 'home_view.html', context)
+
+
+def about_view(request):
+    return render(request, 'about_view.html', {})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error. Please try again')
+            return redirect('login')
+    else:
+        return render(request, 'login_view.html', {})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
+
 
 
