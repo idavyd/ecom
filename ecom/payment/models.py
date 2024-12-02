58ac8c9f3fdb540927f6d2c34268fc633aef05e3
idavyd <ihor.davyydenko@gmail.com>
2024-12-02 22:23:25 +0100

    EP 38 Shipping Date  02/12 home

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime as dt
+
 
 
 class ShippingAddress(models.Model):
@@ -30,9 +33,13 @@ class Order(models.Model):
     amount_paid = models.DecimalField(max_digits=15, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
     is_shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
+
 
     def __str__(self):
         return f'Order - {str(self.id)}'
+
+
 
 
 class OrderItem(models.Model):
@@ -50,6 +57,14 @@ def create_s_address(sender, instance, created, **kwargs):
     if created:
         user_shipping = ShippingAddress(user=instance)
         user_shipping.save()
+
+
+@receiver(pre_save,sender=Order)
+def set_shipped_date_on_update(sender, instance: Order, **kwargs):
+    if instance.pk:
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.is_shipped and not obj.is_shipped:
+            instance.date_shipped = dt.datetime.now()
 
 
 post_save.connect(create_s_address, sender=User)
