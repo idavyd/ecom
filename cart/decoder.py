37802fd3a3b62e1cc09c046eaf057d8f5defdf9d
IDavydenko <idavydenko@actcommodities.com>
2024-12-09 09:22:39 +0100

    ep 47 p3 office 09/12

--- cart/decoder.py
+++ cart/decoder.py
@@ -1,5 +1,5 @@
 from django.contrib.sessions.models import Session
-key = 'wjpbywzcckz69cr6sc5npi9qqks8d3ho'
-k = Session.objects.get(pk='wjpbywzcckz69cr6sc5npi9qqks8d3ho')
+key = '2vl3dorl4aiczt3lkibvfogj1219j3k3'
+k = Session.objects.get(pk='2vl3dorl4aiczt3lkibvfogj1219j3k3')
 k.get_decoded()
 
